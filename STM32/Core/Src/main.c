c07ebecac17dda5efa56df6d114e3970785a8c76
Arhan Busam <arhan.busam@gmail.com>
2026-09-20 21:46:19 +1000

    Implement I2C protocol on STM32

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -1,36 +1,40 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdbool.h>
+#include <string.h>
 
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+typedef enum { KICK_READY, KICK_PULSING, KICK_COOLDOWN } KickState;
 
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define KICK_COOLDOWN_MS 500U
 
 /* USER CODE END PD */
 
@@ -43,11 +47,28 @@
 ADC_HandleTypeDef hadc1;
 ADC_HandleTypeDef hadc2;
 
+I2C_HandleTypeDef hi2c2;
+
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
-
+uint8_t brightness = 50;
+uint8_t RxData = 0x00;
+uint8_t TxData[32] = {0x00};
+/* ADC fills one buffer while the other holds the last complete scan. */
+static uint8_t scan_buffers[2][32] = {{0}};
+static uint32_t working_scan_index = 1U;
+static volatile uint32_t published_scan_index = 0U;
+uint8_t multiplexer1 = 0x00;
+uint8_t multiplexer2 = 0x00;
+volatile bool multiplexer1_ready = true;
+volatile bool multiplexer2_ready = true;
+static volatile uint16_t last_multiplexer_us;
+/* Shared between the main loop and the I2C interrupt. */
+static volatile KickState kick_state = KICK_READY;
+static uint32_t kick_finished_tick;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -57,12 +78,128 @@ static void MX_ADC1_Init(void);
 static void MX_ADC2_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM4_Init(void);
+static void MX_I2C2_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static void service_kick_cooldown(void) {
+  if (kick_state == KICK_PULSING && (TIM1->CR1 & TIM_CR1_CEN) == 0U) {
+    /* One-pulse mode has finished and released PA8. A delayed loop only
+       lengthens the cooldown; never measure it from the pulse start. */
+    kick_finished_tick = HAL_GetTick();
+    kick_state = KICK_COOLDOWN;
+  } else if (kick_state == KICK_COOLDOWN && (uint32_t)(HAL_GetTick() - kick_finished_tick) > KICK_COOLDOWN_MS) {
+    /* Wait past 500 ticks so tick rounding cannot shorten the 500 ms gap.
+       Unsigned subtraction also handles the HAL tick counter wrapping. */
+    kick_state = KICK_READY;
+  }
+}
+
+void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c) {
+  if (hi2c->Instance == I2C2) {
+    HAL_I2C_EnableListen_IT(&hi2c2);
+  }
+}
+
+void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode) {
+  if (hi2c->Instance == I2C2) {
+    if (TransferDirection == I2C_DIRECTION_TRANSMIT) {
+      HAL_I2C_Slave_Seq_Receive_IT(&hi2c2, &RxData, 1, I2C_FIRST_AND_LAST_FRAME);
+    } else {
+      /* ADC must have equal or lower preemption priority than I2C.
+         Snapshot into TxData before ADC can reuse either scan buffer. */
+      uint32_t scan_index = published_scan_index;
+      __DMB();
+      memcpy(TxData, scan_buffers[scan_index], sizeof(TxData));
+      HAL_I2C_Slave_Seq_Transmit_IT(&hi2c2, TxData, sizeof(TxData), I2C_FIRST_AND_LAST_FRAME);
+    }
+  }
+}
+
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c) {
+  if (hi2c->Instance == I2C2) {
+    if (RxData == 0xFF) {
+      if (kick_state == KICK_READY && (TIM1->CR1 & TIM_CR1_CEN) == 0U) {
+        kick_state = KICK_PULSING;
+        __HAL_TIM_SET_COUNTER(&htim1, 1);
+        __HAL_TIM_ENABLE(&htim1);
+      }
+    } else {
+      brightness = RxData;
+      TIM4->CCR1 = (80U * brightness) / 254U;
+    }
+  }
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
+  if (hadc->Instance == ADC1) {
+    uint32_t raw = HAL_ADC_GetValue(hadc);
+    uint8_t value = (uint8_t)((raw * 255U + 2047U) / 4095U);
+    scan_buffers[working_scan_index][multiplexer1] = value;
+    multiplexer1 = (multiplexer1 + 1) % 16;
+    if (multiplexer1 & 0x01) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+    }
+    if (multiplexer1 & 0x02) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+    }
+    if (multiplexer1 & 0x04) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+    }
+    if (multiplexer1 & 0x08) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+    }
+    multiplexer1_ready = true;
+  } else if (hadc->Instance == ADC2) {
+    uint32_t raw = HAL_ADC_GetValue(&hadc2);
+    uint8_t value = (uint8_t)((raw * 255U + 2047U) / 4095U);
+    scan_buffers[working_scan_index][16 + multiplexer2] = value;
+    multiplexer2 = (multiplexer2 + 1) % 16;
+    if (multiplexer2 & 0x01) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+    }
+    if (multiplexer2 & 0x02) {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+    }
+    if (multiplexer2 & 0x04) {
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+    }
+    if (multiplexer2 & 0x08) {
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+    }
+    multiplexer2_ready = true;
+  }
+  if (multiplexer1_ready && multiplexer2_ready) {
+    if (multiplexer1 == 0U && multiplexer2 == 0U) {
+      /* Finish all sample writes before atomically publishing the index.
+         I2C preemption sees either the complete old or complete new scan. */
+      __DMB();
+      published_scan_index = working_scan_index;
+      working_scan_index ^= 1U;
+    }
+    last_multiplexer_us = (uint16_t)__HAL_TIM_GET_COUNTER(&htim2);
+  }
+}
 
 /* USER CODE END 0 */
 
@@ -99,18 +236,51 @@ int main(void)
   MX_ADC2_Init();
   MX_TIM1_Init();
   MX_TIM4_Init();
+  MX_I2C2_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
+  __HAL_TIM_SET_COUNTER(&htim1, 0);
+  SET_BIT(TIM1->CCER, TIM_CCER_CC1E);
+  __HAL_TIM_MOE_ENABLE(&htim1);
+  HAL_I2C_EnableListen_IT(&hi2c2);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+
+  if (HAL_ADCEx_Calibration_Start(&hadc1) != HAL_OK) {
+    Error_Handler();
+  }
+  if (HAL_ADCEx_Calibration_Start(&hadc2) != HAL_OK) {
+    Error_Handler();
+  }
+  if (HAL_TIM_Base_Start(&htim2) != HAL_OK) {
+    Error_Handler();
+  }
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
+    service_kick_cooldown();
+    if (multiplexer1_ready && multiplexer2_ready) {
+      uint16_t now = (uint16_t)__HAL_TIM_GET_COUNTER(&htim2);
+      uint16_t elapsed = (uint16_t)(now - last_multiplexer_us);
+      // Wati 100 us after switching multiplexer before reading ADC
+      if (elapsed > 100U) {
+        multiplexer1_ready = false;
+        multiplexer2_ready = false;
+        if (HAL_ADC_Start_IT(&hadc1) != HAL_OK) {
+          Error_Handler();
+        }
+        if (HAL_ADC_Start_IT(&hadc2) != HAL_OK) {
+          Error_Handler();
+        }
+      }
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
+
   /* USER CODE END 3 */
 }
 
@@ -193,7 +363,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -240,7 +410,7 @@ static void MX_ADC2_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -248,6 +418,40 @@ static void MX_ADC2_Init(void)
   /* USER CODE BEGIN ADC2_Init 2 */
 
   /* USER CODE END ADC2_Init 2 */
+
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 400000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 110;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
 
 }
 
@@ -301,9 +505,9 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.OCMode = TIM_OCMODE_PWM2;
   sConfigOC.Pulse = 1;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_LOW;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
@@ -327,6 +531,51 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 7;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 65535;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
 
 }
 
@@ -439,12 +688,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB10 PB11 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
   /*Configure GPIO pins : PA9 PA10 PA11 PA12
                            PA15 */
   GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12
@@ -473,8 +716,7 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
@@ -489,8 +731,9 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
